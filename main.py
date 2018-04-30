2598a117223789b5ce843b690c55b244b2409533
Michel <cassard@mit.edu>
2018-04-30 15:17:44 -0400

    s

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 28 21:08:46 2018
 
-@author: youssefberrada
-"""
 
 import seaborn as sns
 import numpy as np
@@ -40,8 +34,8 @@ neurons = [256, 256, 32, 1]
 dropout = 0.3
 decay = 0.5
 epochs = 90
-os.chdir("/Users/youssefberrada/Dropbox (MIT)/15.961 Independant Study/Data")
-#os.chdir("/Users/michelcassard/Dropbox (MIT)/15.960 Independant Study/Data")
+#os.chdir("/Users/youssefberrada/Dropbox (MIT)/15.961 Independant Study/Data")
+os.chdir("/Users/michelcassard/Dropbox (MIT)/15.960 Independant Study/Data")
 file = 'FX-5.xlsx'
 # Load spreadsheet
 xl = pd.ExcelFile(file)
@@ -51,32 +45,32 @@ df_close=np.array(close.parse(0))
 
 def get_stock_data(stock_name, ma=[]):
     """
-    Return a dataframe of that stock and normalize all the values. 
+    Return a dataframe of that stock and normalize all the values.
     (Optional: create moving average)
-    
+
     """
     df = xl.parse(stock_name)
     df.drop(['VOLUME'], 1, inplace=True)
     df.set_index('Date', inplace=True)
-    
+
     # Renaming all the columns so that we can use the old version code
     df.rename(columns={'OPEN': 'Open', 'HIGH': 'High', 'LOW': 'Low', 'NUMBER_TICKS': 'Volume', 'LAST_PRICE': 'Adj Close'}, inplace=True)
      # Percentage change
     df['Pct'] = df['Adj Close'].pct_change()
     df.dropna(inplace=True)
-    
-    # Moving Average    
+
+    # Moving Average
     if ma != []:
         for moving in ma:
             df['{}ma'.format(moving)] = df['Adj Close'].rolling(window=moving).mean()
     df.dropna(inplace=True)
-    
-  
+
+
     # Move Adj Close to the rightmost for the ease of training
     adj_close = df['Adj Close']
     df.drop(labels=['Adj Close'], axis=1, inplace=True)
     df = pd.concat([df, adj_close], axis=1)
-      
+
     return df
 
 
@@ -89,21 +83,21 @@ def plot_stock(df):
     plt.plot(df['Pct'], color='blue', label='Percentage change')
     plt.legend(loc='best')
     plt.show()
-    
-    
+
+
 def load_data(stock,normalize,seq_len,split,ma):
     amount_of_features = len(stock.columns)
     print ("Amount of features = {}".format(amount_of_features))
     sequence_length = seq_len+1
     result_train = []
     result_test= []
-    row = round(split * stock.shape[0]) 
+    row = round(split * stock.shape[0])
     df_train=stock[0:row].copy()
     print ("Amount of training data = {}".format(df_train.shape[0]))
     df_test=stock[row:len(stock)].copy()
     print ("Amount of testing data = {}".format(df_test.shape[0]))
 
-    
+
     if normalize:
         #Training
         min_max_scaler = preprocessing.MinMaxScaler()
@@ -115,7 +109,7 @@ def load_data(stock,normalize,seq_len,split,ma):
         df_train['Pct'] = min_max_scaler.fit_transform(df_train['Pct'].values.reshape(-1,1))
         if ma != []:
             for moving in ma:
-                df_train['{}ma'.format(moving)] = min_max_scaler.fit_transform(df_train['{}ma'.format(moving)].values.reshape(-1,1))  
+                df_train['{}ma'.format(moving)] = min_max_scaler.fit_transform(df_train['{}ma'.format(moving)].values.reshape(-1,1))
         #Test
         df_test['Open'] = min_max_scaler.fit_transform(df_test['Adj Close'].values.reshape(-1,1))
         df_test['High'] = min_max_scaler.fit_transform(df_test['Adj Close'].values.reshape(-1,1))
@@ -125,34 +119,34 @@ def load_data(stock,normalize,seq_len,split,ma):
         df_test['Pct'] = min_max_scaler.fit_transform(df_test['Pct'].values.reshape(-1,1))
         if ma != []:
             for moving in ma:
-                df_test['{}ma'.format(moving)] = min_max_scaler.fit_transform(df_test['{}ma'.format(moving)].values.reshape(-1,1))  
-       
+                df_test['{}ma'.format(moving)] = min_max_scaler.fit_transform(df_test['{}ma'.format(moving)].values.reshape(-1,1))
+
     #Training
     data_train = df_train.as_matrix()
-    for index in range(len(data_train) - sequence_length): 
-        result_train.append(data_train[index: index + sequence_length]) 
+    for index in range(len(data_train) - sequence_length):
+        result_train.append(data_train[index: index + sequence_length])
     train = np.array(result_train)
     X_train = train[:, :-1].copy() # all data until day m
     y_train = train[:, -1][:,-1].copy() # day m + 1 adjusted close price
 
     #Test
     data_test = df_test.as_matrix()
-    for index in range(len(data_test) - sequence_length): 
-        result_test.append(data_test[index: index + sequence_length]) 
+    for index in range(len(data_test) - sequence_length):
+        result_test.append(data_test[index: index + sequence_length])
     test = np.array(result_train)
     X_test = test[:, :-1].copy()
     y_test = test[:, -1][:,-1].copy()
 
-    
+
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], amount_of_features))
     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], amount_of_features))
 
     return [X_train, y_train, X_test, y_test]
-    
+
 
 def build_model(shape, neurons, dropout, decay):
     model = Sequential()
-   
+
     model.add(LSTM(neurons[0], input_shape=(shape[0], shape[1]), return_sequences=True))
     model.add(Dropout(dropout))
 
@@ -161,7 +155,7 @@ def build_model(shape, neurons, dropout, decay):
 
     model.add(Dense(neurons[2],kernel_initializer="uniform",activation='relu'))
     model.add(Dense(neurons[3],kernel_initializer="uniform",activation='linear'))
-    
+
     adam = keras.optimizers.Adam(decay=decay)
     model.compile(loss='mse',optimizer='adam', metrics=['accuracy'])
     model.summary()
@@ -170,7 +164,7 @@ def build_model(shape, neurons, dropout, decay):
 
 def build_model_CNN(shape, neurons, dropout, decay):
     model = Sequential()
-    model.add(Convolution1D(input_shape = (shape[0], shape[1]), 
+    model.add(Convolution1D(input_shape = (shape[0], shape[1]),
                         nb_filter=64,
                         filter_length=2,
                         border_mode='valid',
@@ -178,7 +172,7 @@ def build_model_CNN(shape, neurons, dropout, decay):
                         subsample_length=1))
     model.add(MaxPooling1D(pool_length=2))
 
-    model.add(Convolution1D(input_shape = (shape[0], shape[1]), 
+    model.add(Convolution1D(input_shape = (shape[0], shape[1]),
                         nb_filter=64,
                         filter_length=2,
                         border_mode='valid',
@@ -240,7 +234,7 @@ def denormalize(stock_name, normalized_value,split=0.7,predict=True):
     df = xl.parse(stock_name)
     df.drop(['VOLUME'], 1, inplace=True)
     df.set_index('Date', inplace=True)
-    
+
     # Renaming all the columns so that we can use the old version code
     df.rename(columns={'OPEN': 'Open', 'HIGH': 'High', 'LOW': 'Low', 'NUMBER_TICKS': 'Volume', 'LAST_PRICE': 'Adj Close'}, inplace=True)
 
@@ -248,18 +242,18 @@ def denormalize(stock_name, normalized_value,split=0.7,predict=True):
     df.dropna(inplace=True)
     df = df['Adj Close'].values.reshape(-1,1)
     normalized_value = normalized_value.reshape(-1,1)
-    
-    row = round(split * df.shape[0]) 
+
+    row = round(split * df.shape[0])
     if predict:
         df_p=df[0:row].copy()
     else:
         df_p=df[row:len(df)].copy()
-    
+
     #return df.shape, p.shape
     max_df=np.max(df_p)
     min_df=np.min(df_p)
     new=normalized_value*(max_df-min_df)+min_df
-      
+
     return new
 
 def portfolio(currency_list,file = 'FX-5.xlsx',seq_len = 22,shape = [seq_len, 9, 1],neurons = [256, 256, 32, 1],dropout = 0.3,decay = 0.5,
@@ -308,8 +302,8 @@ def MarkowitzWithTransactionsCost(n,mu,GT,x0,w,gamma,f,g):
         # Defines the variables. No shortselling is allowed.
         x = M.variable("x", n, Domain.greaterThan(0.0))
 
-        # Additional "helper" variables 
-        z = M.variable("z", n, Domain.unbounded())   
+        # Additional "helper" variables
+        z = M.variable("z", n, Domain.unbounded())
         # Binary variables
         y = M.variable("y", n, Domain.binary())
 
@@ -322,7 +316,7 @@ def MarkowitzWithTransactionsCost(n,mu,GT,x0,w,gamma,f,g):
         # Imposes a bound on the risk
         M.constraint('risk', Expr.vstack( gamma,Expr.mul(GT,x)), Domain.inQCone())
 
-        # z >= |x-x0| 
+        # z >= |x-x0|
         M.constraint('buy', Expr.sub(z,Expr.sub(x,x0)),Domain.greaterThan(0.0))
         M.constraint('sell', Expr.sub(z,Expr.sub(x0,x)),Domain.greaterThan(0.0))
         # Alternatively, formulate the two constraints as
@@ -331,14 +325,12 @@ def MarkowitzWithTransactionsCost(n,mu,GT,x0,w,gamma,f,g):
         # Constraints for turning y off and on. z-diag(u)*y<=0 i.e. z_j <= u_j*y_j
         M.constraint('y_on_off', Expr.sub(z,Expr.mulElm(u,y)), Domain.lessThan(0.0))
 
-        # Integer optimization problems can be very hard to solve so limiting the 
+        # Integer optimization problems can be very hard to solve so limiting the
         # maximum amount of time is a valuable safe guard
-        M.setSolverParam('mioMaxTime', 180.0) 
+        M.setSolverParam('mioMaxTime', 180.0)
         M.solve()
 
         return (np.dot(mu,x.level()), x.level())
-
-
 
 
 def rebalance(n,previous_prices,x0,w,mu,gamma=1):
@@ -347,7 +339,6 @@ def rebalance(n,previous_prices,x0,w,mu,gamma=1):
     g = n*[0.001]
     weights=MarkowitzWithTransactionsCost(n,mu,GT,x0,w,gamma,f,g)
     return weights
-
 
 
 # Backtesting using rebalancing function for weights
@@ -400,5 +391,3 @@ def plot_result(stock_name, normalized_value_p, normalized_value_y_test):
     plt2.xlabel('5 Min ahead Forecast')
     plt2.ylabel('Price')
     plt2.show()
-
-
