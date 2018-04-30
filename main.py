74ea6fde927117283b238e7d07aaaecad2bee1fa
Youssef Berrada <yberrada@mit.edu>
2018-04-30 13:03:06 -0400

    Main clean

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from pandas import datetime
 import math, time
 import itertools
 from sklearn import preprocessing
+import datetime
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 from keras.models import Sequential
@@ -28,10 +29,10 @@ from statistics import mean
 from keras import backend as K
 from keras.layers.convolutional import Convolution1D, MaxPooling1D
 from keras.layers.core import Flatten
+import matplotlib.pyplot as plt
 from mosek.fusion import *
 import pylab
 import random
-
 
 seq_len = 22
 shape = [seq_len, 9, 1]
@@ -39,11 +40,14 @@ neurons = [256, 256, 32, 1]
 dropout = 0.3
 decay = 0.5
 epochs = 90
-#os.chdir("/Users/youssefberrada/Dropbox (MIT)/15.961 Independant Study/Data")
-os.chdir("/Users/michelcassard/Dropbox (MIT)/15.960 Independant Study/Data")
+os.chdir("/Users/youssefberrada/Dropbox (MIT)/15.961 Independant Study/Data")
+#os.chdir("/Users/michelcassard/Dropbox (MIT)/15.960 Independant Study/Data")
 file = 'FX-5.xlsx'
 # Load spreadsheet
 xl = pd.ExcelFile(file)
+close = pd.ExcelFile('close.xlsx')
+df_close=np.array(close.parse(0))
+
 
 def get_stock_data(stock_name, ma=[]):
     """
@@ -75,7 +79,6 @@ def get_stock_data(stock_name, ma=[]):
       
     return df
 
-df_GBP=get_stock_data("GBP Curncy",  ma=[50, 100, 200])
 
 def plot_stock(df):
     print(df.head())
@@ -87,12 +90,11 @@ def plot_stock(df):
     plt.legend(loc='best')
     plt.show()
     
-plot_stock(df_GBP)   
     
 def load_data(stock,normalize,seq_len,split,ma):
     amount_of_features = len(stock.columns)
     print ("Amount of features = {}".format(amount_of_features))
-    sequence_length = seq_len + 1 
+    sequence_length = seq_len+1
     result_train = []
     result_test= []
     row = round(split * stock.shape[0]) 
@@ -105,9 +107,9 @@ def load_data(stock,normalize,seq_len,split,ma):
     if normalize:
         #Training
         min_max_scaler = preprocessing.MinMaxScaler()
-        df_train['Open'] = min_max_scaler.fit_transform(df_train.Open.values.reshape(-1,1))
-        df_train['High'] = min_max_scaler.fit_transform(df_train.High.values.reshape(-1,1))
-        df_train['Low'] = min_max_scaler.fit_transform(df_train.Low.values.reshape(-1,1))
+        df_train['Open'] = min_max_scaler.fit_transform(df_train['Adj Close'].values.reshape(-1,1))
+        df_train['High'] = min_max_scaler.fit_transform(df_train['Adj Close'].values.reshape(-1,1))
+        df_train['Low'] = min_max_scaler.fit_transform(df_train['Adj Close'].values.reshape(-1,1))
         df_train['Volume'] = min_max_scaler.fit_transform(df_train.Volume.values.reshape(-1,1))
         df_train['Adj Close'] = min_max_scaler.fit_transform(df_train['Adj Close'].values.reshape(-1,1))
         df_train['Pct'] = min_max_scaler.fit_transform(df_train['Pct'].values.reshape(-1,1))
@@ -115,16 +117,16 @@ def load_data(stock,normalize,seq_len,split,ma):
             for moving in ma:
                 df_train['{}ma'.format(moving)] = min_max_scaler.fit_transform(df_train['{}ma'.format(moving)].values.reshape(-1,1))  
         #Test
-        df_test['Open'] = min_max_scaler.fit_transform(df_test.Open.values.reshape(-1,1))
-        df_test['High'] = min_max_scaler.fit_transform(df_test.High.values.reshape(-1,1))
-        df_test['Low'] = min_max_scaler.fit_transform(df_test.Low.values.reshape(-1,1))
+        df_test['Open'] = min_max_scaler.fit_transform(df_test['Adj Close'].values.reshape(-1,1))
+        df_test['High'] = min_max_scaler.fit_transform(df_test['Adj Close'].values.reshape(-1,1))
+        df_test['Low'] = min_max_scaler.fit_transform(df_test['Adj Close'].values.reshape(-1,1))
         df_test['Volume'] = min_max_scaler.fit_transform(df_test.Volume.values.reshape(-1,1))
         df_test['Adj Close'] = min_max_scaler.fit_transform(df_test['Adj Close'].values.reshape(-1,1))
         df_test['Pct'] = min_max_scaler.fit_transform(df_test['Pct'].values.reshape(-1,1))
         if ma != []:
             for moving in ma:
                 df_test['{}ma'.format(moving)] = min_max_scaler.fit_transform(df_test['{}ma'.format(moving)].values.reshape(-1,1))  
-     
+       
     #Training
     data_train = df_train.as_matrix()
     for index in range(len(data_train) - sequence_length): 
@@ -145,17 +147,12 @@ def load_data(stock,normalize,seq_len,split,ma):
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], amount_of_features))
     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], amount_of_features))
 
-    return [X_train, y_train, X_test, y_test]   
-    
-    
-X_train, y_train, X_test, y_test = load_data(df_GBP,True,seq_len,split=0.7,ma=[50, 100, 200])  
+    return [X_train, y_train, X_test, y_test]
     
 
 def build_model(shape, neurons, dropout, decay):
     model = Sequential()
-
-    #model.add(Dense(neurons[0],activation="relu", input_shape=(shape[0], shape[1])))
-    
+   
     model.add(LSTM(neurons[0], input_shape=(shape[0], shape[1]), return_sequences=True))
     model.add(Dropout(dropout))
 
@@ -205,8 +202,6 @@ def build_model_CNN(shape, neurons, dropout, decay):
 
 model = build_model_CNN(shape, neurons, dropout, decay)
 
-model.fit(X_train,y_train,batch_size=512,epochs=epochs,validation_split=0.3,verbose=1)
-
 def model_score(model, X_train, y_train, X_test, y_test):
     trainScore = model.evaluate(X_train, y_train, verbose=0)
     print('Train Score: %.5f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
@@ -214,8 +209,6 @@ def model_score(model, X_train, y_train, X_test, y_test):
     testScore = model.evaluate(X_test, y_test, verbose=0)
     print('Test Score: %.5f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
     return trainScore[0], testScore[0]
-
-model_score(model, X_train, y_train, X_test, y_test)
 
 def percentage_difference(model, X_test, y_test):
     percentage_diff=[]
@@ -230,10 +223,6 @@ def percentage_difference(model, X_test, y_test):
 
 
 
-
-p = percentage_difference(model, X_test, y_test)
-
-
 def plot_result_norm(stock_name, normalized_value_p, normalized_value_y_test):
     newp=normalized_value_p
     newy_test=normalized_value_y_test
@@ -245,14 +234,9 @@ def plot_result_norm(stock_name, normalized_value_p, normalized_value_y_test):
     plt2.ylabel('Price')
     plt2.show()
 
-plot_result_norm("GBP Curncy", p, y_test)
-
 
 def denormalize(stock_name, normalized_value,split=0.7,predict=True):
-    """
-    Return a dataframe of that stock and normalize all the values. 
-    (Optional: create moving average)
-    """
+
     df = xl.parse(stock_name)
     df.drop(['VOLUME'], 1, inplace=True)
     df.set_index('Date', inplace=True)
@@ -272,9 +256,9 @@ def denormalize(stock_name, normalized_value,split=0.7,predict=True):
         df_p=df[row:len(df)].copy()
     
     #return df.shape, p.shape
-    mean_df=np.mean(df_p)
-    std_df=np.std(df_p)
-    new=normalized_value*mean_df+std_df
+    max_df=np.max(df_p)
+    min_df=np.min(df_p)
+    new=normalized_value*(max_df-min_df)+min_df
       
     return new
 
@@ -292,11 +276,12 @@ def portfolio(currency_list,file = 'FX-5.xlsx',seq_len = 22,shape = [seq_len, 9,
         if mini>p.size:
             mini=p.size
         if i==0:
-            predict=p.copy()
+            predict=newp.copy()
         else:
-            predict=np.hstack((predict[0:mini],p[0:mini]))
+            predict=np.hstack((predict[0:mini],newp[0:mini]))
         i+=1
     return predict
+
 
 
 currency_list=[ 'GBP Curncy',
@@ -307,30 +292,11 @@ currency_list=[ 'GBP Curncy',
  'SEK Curncy',
  'AUD Curncy',
  'CHF Curncy',
- 'NOK Curncy']
-#currency_list=['JPY Curncy']
-predictcur=portfolio(currency_list,file = 'FX-5.xlsx',seq_len = 22,shape = [seq_len, 9, 1],neurons = [256, 256, 32, 1],dropout = 0.3,decay = 0.5,
-              epochs = 1,ma=[50, 100, 200],split=0.7)
+ 'NOK Curncy',
+ 'ZAR Curncy']
+predictcur=portfolio(currency_list,file = 'FX-5-merg.xlsx',seq_len = 22,shape = [seq_len, 9, 1],neurons = [256, 256, 32, 1],dropout = 0.3,decay = 0.5,
+              epochs = 90,ma=[50, 100, 200],split=0.7)
 
-
-"""
-    Description:
-        Extends the basic Markowitz model with a market cost term.
-
-    Input:
-        n: Number of assets
-        mu: An n dimensional vector of expected returns
-        GT: A matrix with n columns so (GT')*GT  = covariance matrix
-        x0: Initial holdings 
-        w: Initial cash holding
-        gamma: Maximum risk (=std. dev) accepted
-        f: If asset j is traded then a fixed cost f_j must be paid
-        g: If asset j is traded then a cost g_j must be paid for each unit traded
-
-    Output:
-       Optimal expected return and the optimal portfolio     
-
-"""
 def MarkowitzWithTransactionsCost(n,mu,GT,x0,w,gamma,f,g):
     # Upper bound on the traded amount
     w0 = w+sum(x0)
@@ -370,12 +336,6 @@ def MarkowitzWithTransactionsCost(n,mu,GT,x0,w,gamma,f,g):
         M.setSolverParam('mioMaxTime', 180.0) 
         M.solve()
 
-        print("\n-----------------------------------------------------------------------------------");
-        print('Markowitz portfolio optimization with transactions cost')
-        print("-----------------------------------------------------------------------------------\n");
-        print('Expected return: %.4e Std. deviation: %.4e Transactions cost: %.4e' % \
-              (np.dot(mu,x.level()),gamma,np.dot(f,y.level())+np.dot(g,z.level())))
-
         return (np.dot(mu,x.level()), x.level())
 
 
@@ -388,7 +348,6 @@ def rebalance(n,previous_prices,x0,w,mu,gamma=1):
     weights=MarkowitzWithTransactionsCost(n,mu,GT,x0,w,gamma,f,g)
     return weights
 
-rebalance(9,dq,mu=predictcur[1],x0=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],w=1,gamma=1)
 
 
 # Backtesting using rebalancing function for weights
@@ -398,35 +357,38 @@ def log_diff(data):
 
 def backtest(prices, predictions, initial_weights):
     t_prices = len(prices[1,:])
-    t_predictions = len(predictions[:,1])
+    t_predictions = len(predictions)
     length_past = t_prices - t_predictions
     returns = np.apply_along_axis(log_diff, 1, prices)
     prediction_return = []
+    port_return=1
     for k in range(t_predictions):
-        prediction_return.append(np.log(predictions[k]/prices[:,length_past+k]))
+        prediction_return.append(np.log(predictions[k]/prices[:,length_past+k-1]))
     weights = initial_weights
     portfolio_return = []
     prev_weight = weights
     for i in range(0,t_predictions-1):
+        print(i)
         predicted_return = prediction_return[i]
-        previous_return = returns[:,length_past+i]
-        previous_returns = returns[:,0:length_past+i]
+        previous_return = returns[:,length_past+i-1]
+        previous_returns = returns[:,0:(length_past+i-1)]
         if i==0:
-            new_weight = rebalance_y(3,previous_returns,mu=predicted_return.tolist(),x0=prev_weight,w=1,gamma=0.5)
+            new_weight = rebalance(10,previous_returns,mu=predicted_return.tolist(),x0=prev_weight,w=1,gamma=0.05)
         else:
-            new_weight = rebalance_y(3,previous_returns,mu=predicted_return.tolist(),x0=prev_weight,w=0,gamma=0.5)
-        period_return = new_weight*np.log(prices[:,length_past+i+1]/prices[:,length_past+i])
-        portfolio_return.append(np.sum(period_return))
+            new_weight = rebalance(10,previous_returns,mu=predicted_return.tolist(),x0=prev_weight,w=np.sum(period_return),gamma=0.05)
+        period_return = new_weight*np.log(prices[:,length_past+i]/prices[:,length_past+i-1])
+        port_return=port_return*(1+np.sum(period_return))
+        portfolio_return.append(port_return)
         prev_weight = new_weight
+        print(new_weight)
     return portfolio_return
 
 
 
-x = backtest(dq.T, predictcur, np.repeat(1/10,10))
+x=backtest(df_close.T, predictcur, np.repeat(1/10,10))
 
-
-
-
+plt.plot(x)
+x[-1]
 
 def plot_result(stock_name, normalized_value_p, normalized_value_y_test):
     newp = denormalize(stock_name, normalized_value_p,predict=True)
@@ -439,5 +401,3 @@ def plot_result(stock_name, normalized_value_p, normalized_value_y_test):
     plt2.ylabel('Price')
     plt2.show()
 
-plot_result("GBP Curncy", p, y_test)
-å
